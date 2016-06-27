321a5aabbc2a049797e4cdb6ee2af9cdbd5d3c8c
Omar Sahyoun <osahyoun@gmail.com>
2016-06-27 12:04:08 +0100

    Initiali commit

---
+++ app/assets/javascripts/components/foo.js.jsx
@@ -0,0 +1,5 @@
+const Foo = (props) => {
+  return <h1>Hello World</h1>
+};
+
+ReactDOM.render(<Foo />, document.getElementById('root'));
---
+++ config/initializers/cookies_serializer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# Specify a serializer for the signed and encrypted cookie jars.
+# Valid options are :json, :marshal, and :hybrid.
+Rails.application.config.action_dispatch.cookies_serializer = :json
+
+
+#Rails.application.config.react.jsx_transform_options = {
+  #presets: ['es2015']
+#}
