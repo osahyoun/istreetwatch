d1146e242a9d9bdc0dfb5eefd971cdf93b4e15f3
James Rees <jarees@gmail.com>
2016-06-29 22:23:41 +0100

    Add below pledge form content

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,6 +1,7 @@
 class Pledge < ActiveRecord::Base
 
   validates_presence_of :name, :email
+  validates_format_of       :email,    :with => /\A[^@\s]+@[^@\s]+\.[^@\s]+\Z/i
 
   class << self
     def cached_count
