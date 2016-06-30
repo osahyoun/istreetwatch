635c9c42ba17b6310307132c0f007e24cdd3d12d
Omar Sahyoun <osahyoun@gmail.com>
2016-07-01 01:29:40 +0200

    Redirect

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,16 +1,8 @@
 class PledgesController < ApplicationController
   def create
     @pledge = Pledge.new(pledge_params)
-
-    respond_to do |format|
-      if @pledge.save
-        format.html { redirect_to thankyou_pledges_path }
-        format.json { render json: @pledge }
-      else
-        format.html { render :new }
-        format.json { render json: @pledge.errors, status: :unprocessable_entity }
-      end
-    end
+    @pledge.save
+    redirect_to thankyou_pledges_path
   end
 
   private
