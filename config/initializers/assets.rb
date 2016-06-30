cb6c7313f8a5eac449999e79bdd8ea0bf8d80636
Omar Sahyoun <osahyoun@gmail.com>
2016-06-30 20:05:30 +0200

    Use asset path helper

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '2.0'
+Rails.application.config.assets.compile = true
+Rails.application.config.assets.digest = true
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
