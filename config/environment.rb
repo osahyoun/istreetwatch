fd8fd1550a23da584db743e78fc00297ef1e3765
Sarah Skilton <skiltoni@gmail.com>
2017-02-01 18:50:49 +0000

    Customise errors

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Remove field_with_errors wrapper
+ActionView::Base.field_error_proc = Proc.new do |html_tag, object|
+  html = Nokogiri::HTML::DocumentFragment.parse(html_tag)
+  html = html.at_css("input") || html.at_css("textarea") || html.at_css("select")
+  unless html.nil?
+    css_class = html['class'] || ""
+    html['class'] = css_class.split.push("error").join(' ')
+    html_tag = html.to_s
+  end
+  html_tag.html_safe
+end
