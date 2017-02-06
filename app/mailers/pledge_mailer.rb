c74de2de90154e26a7107eab32cc9e60365dbbf3
Sarah Skilton <skiltoni@gmail.com>
2017-02-06 15:17:41 +0000

    Fix typo

--- app/mailers/pledge_mailer.rb
+++ app/mailers/pledge_mailer.rb
@@ -2,7 +2,7 @@ class PledgeMailer < ApplicationMailer
   default from: "info@bitsalad.co"
 
   def thank_you_email( pledge )
-    @name = pedge.name
-    mail(to: pedge.email, subject: "Thank you!")
+    @name = pledge.name
+    mail(to: pledge.email, subject: "Thank you!")
   end
 end
