04bf3c0b4bc4d9806b13fef59638ffa659c1edfd
Sarah Skilton <skiltoni@gmail.com>
2017-02-12 19:27:59 +0000

    Set elasticsearch client for production only

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,3 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+if Rails.env.production?
+  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+end
